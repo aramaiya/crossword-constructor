1ab6f18916754d5847d8fa7a0a87a0ba6e0fbeec
Amit Ramaiya <amitramaiya@Amits-MBP.attlocal.net>
2017-12-27 19:15:08 -0500

    move new dialog to toolbar

--- src/components/Builder.vue
+++ src/components/Builder.vue
@@ -2,10 +2,6 @@
   <div class="container">
     <toolbar></toolbar>
     <grid></grid>
-    <div class = "overlay new-puzzle" :hidden="!newPuzzleVisible">
-      <button>Create</button>
-      <button @click="cancelClicked">Cancel</button>
-    </div>
   </div>
 </template>
 
@@ -18,21 +14,14 @@ export default Vue.extend({
   name: "Builder",
   data() {
     return {
-      newPuzzleVisible: false
+      
     };
   },
   created() {},
   mounted() {
-    $eventService.subscribe(EventService.Events.NewPuzzleRequest, this.showNewPuzzle, this);
-
   },
   methods: {
-    showNewPuzzle() {
-      this.newPuzzleVisible = true;
-    },
-    cancelClicked() {
-      this.newPuzzleVisible = false;
-    }
+    
   },
   components: {
     Toolbar,
@@ -44,14 +33,5 @@ export default Vue.extend({
 <style scoped lang="scss">
   .container {
     position: relative;
-  }
-  .overlay {
-    position: absolute;
-    top: 0px;
-    width: 100%;
-    height: 100%
-  }
-  .new-puzzle {
-    background: lightblue;
   }
 </style>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -5,31 +5,48 @@
         <button v-on:click="clearClicked">Clear</button>
         <input type="radio" name="mode" checked>Fill</input>
         <input type="radio" name="mode">Draw</input>
+        <div class="overlay new-puzzle" :hidden="!newPuzzleVisible">
+            <button>Create</button>
+            <button @click="cancelClicked">Cancel</button>
+        </div>
     </div>
 </template>
 <script lang="ts">
-import {$eventService, EventService} from '../infra/event-service';
+import { $eventService, EventService } from "../infra/event-service";
 import Vue from "vue";
 export default Vue.extend({
-    name: "Toolbar",
-    data() {
-        return {
-            mode: 0
-        }
+  name: "Toolbar",
+  data() {
+    return {
+      newPuzzleVisible: false,
+      mode: 0
+    };
+  },
+  methods: {
+    cancelClicked() {
+      this.newPuzzleVisible = false;
     },
-    methods: {
-        newPuzzleClicked() {
-            $eventService.fire(EventService.Events.NewPuzzleRequest);
-        },
-        undoClicked() {
-            $eventService.fire(EventService.Events.UndoRequest);
-        },
-        clearClicked() {
-            $eventService.fire(EventService.Events.ClearRequest);
-        }
+    newPuzzleClicked() {
+      this.newPuzzleVisible = true;
+    },
+    undoClicked() {
+      $eventService.fire(EventService.Events.UndoRequest);
+    },
+    clearClicked() {
+      $eventService.fire(EventService.Events.ClearRequest);
     }
+  }
 });
 </script>
 <style lang="scss">
-
+.overlay {
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+}
+.new-puzzle {
+  background: lightblue;
+}
 </style>
